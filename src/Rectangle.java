18e9a3926520e498532525abb7bca04f81afa27a
Augusto Batista <augusto.batista@ou.edu>
2019-02-21 13:21:27 -0600

    Finished Rectangle methods

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,19 +1,23 @@
 
 public class Rectangle extends Polygon{
 
-	double height;
-	double width;
+	private double height;
+	private double width;
 	
 	public Rectangle(String id, double height, double width) {
+		super(id);
 		this.height = height;
 		this.width = width;
+		
+		sideLengths.add(height);
+		sideLengths.add(width);
 	}
-	
 	public double getArea(){
 		return height*width;
 	}
-	
-	public String getShape() {
+
+	@Override
+	public String getShapeType() {
 		return "Rectangle";
 	}
 	
